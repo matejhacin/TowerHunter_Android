49e7c1f11d3d089f1bee06fdc0485533c83f6810
Matej Hacin <matej.hacin@gmail.com>
2020-12-11 12:44:26 +0100

    project setup

---
+++ app/src/main/java/com/eles/towerhunter/views/splash/SplashActivity.kt
@@ -0,0 +1,10 @@
+package com.eles.towerhunter.views.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
